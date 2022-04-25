38c8c42b15f4c103ba6c449faa82b24b7046d561
Noah Maktabi <61517594+noah11m@users.noreply.github.com>
2022-04-25 09:24:25 +0200

    Server configuration

--- OAuth/Config.cs
+++ OAuth/Config.cs
@@ -9,18 +9,31 @@ namespace OAuth
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
             {
-                new IdentityResources.OpenId()
+                new ApiScope("oauth2", "OAuth")
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
-
         public static IEnumerable<Client> Clients =>
-            new Client[]
-            { };
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "oauth2" }
+                }
+            };
     }
 }
